d0b328862fc99bc6c66aca46ff090fa43ea5a227
Anastasia CHANBOUR <anastasiachambour@gmail.com>
2021-11-06 17:56:39 +0000

    fixed parser

--- syntaxtree.py
+++ syntaxtree.py
@@ -1,21 +1,20 @@
 class TreeNode():
-    def __init__(self, left, right):
+    def __init__(self, token, left, right):
+        self.token = token
         self.left = left
         self.right = right
 
 
 class InfixExpr(TreeNode):
     def __init__(self, left, operator, right):
-        super().__init__(left, right)
-        print(left, operator.text, right)
+        super().__init__(operator, left, right)
 
 
 class UnaryExpr(TreeNode):
     def __init__(self, operator, expr):
-        super().__init__(expr, None)
-        print(operator.text, expr)
+        super().__init__(operator, expr, None)
 
 
 class NumberLiteral(TreeNode):
     def __init__(self, number):
-        super().__init__(None, None)
+        super().__init__(number, None, None)
