0571d2f2926770436f4421046bbee678b93a7618
Anastasia CHANBOUR <anastasiachambour@gmail.com>
2021-11-06 23:25:48 +0000

    Revert "improved scanner"
    This reverts commit 3018dcd10badcb613610f41567e7a1a4b65b83ec.

--- lexer.py
+++ lexer.py
@@ -91,9 +91,9 @@ class Tokenizer():
 
             # dispatch to scanner helper functions
             if arg[i].isdigit():
-                token = self.number(arg[i:-1])
+                token = self.number(arg[i:])
             elif arg[i] in "+-*/()":
-                token = self.symbol(arg[i:-1])
+                token = self.symbol(arg[i:])
 
             # add token and advance the scanner head
             if token:
